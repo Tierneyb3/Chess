728c62dac54aeecc3d0df5f33863a8456072fa0f
Tierneyb3 <108205423+Tierneyb3@users.noreply.github.com>
2025-12-15 18:52:45 -0500

    Update TakenPiecesPanel.java

--- Chess/src/com/chess/gui/TakenPiecesPanel.java
+++ Chess/src/com/chess/gui/TakenPiecesPanel.java
@@ -1,43 +1,48 @@
 package com.chess.gui;
-import com.chess.engine.board.Move;
-import com.chess.engine.pieces.Piece;
-import com.google.common.primitives.Ints;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.Piece;
+import com.chess.gui.Table.MoveLog;
+import com.google.common.primitives.Ints;
 
 public class TakenPiecesPanel extends JPanel {
 
-    private final JPanel northPanel;
-    private final JPanel southPanel;
+    private final JPanel whitePanel;
+    private final JPanel blackPanel;
     private static final Color PANEL_COLOR = Color.decode("0xFDF36");
-    private static final Dimension TAKEN_PIECES_DIMENSION = new Dimension(40, 80);
+    private static final Dimension TAKEN_PIECES_DIMENSION = new Dimension(400, 80);
     private static final EtchedBorder PANEL_BORDER = new EtchedBorder(EtchedBorder.RAISED);
 
     public TakenPiecesPanel() {
         super(new BorderLayout());
         setBackground(PANEL_COLOR);
         setBorder(PANEL_BORDER);
-        this.northPanel = new JPanel(new GridLayout(8, 2));
-        this.southPanel = new JPanel(new GridLayout(8, 2));
-        this.northPanel.setBackground(PANEL_COLOR);
-        this.southPanel.setBackground(PANEL_COLOR);
-        this.add(this.northPanel, BorderLayout.NORTH);
-        this.add(this.southPanel, BorderLayout.SOUTH);
+        this.whitePanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
+        this.blackPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
+        this.whitePanel.setBackground(PANEL_COLOR);
+        this.blackPanel.setBackground(PANEL_COLOR);
+        this.add(this.whitePanel, BorderLayout.NORTH);
+        this.add(this.blackPanel, BorderLayout.SOUTH);
         setPreferredSize(TAKEN_PIECES_DIMENSION);
     }
 
-    public void redo(final Table.MoveLog moveLog) {
-        this.southPanel.removeAll();
-        this.northPanel.removeAll();
+    public void redo(final MoveLog moveLog) {
+        this.blackPanel.removeAll();
+        this.whitePanel.removeAll();
 
         final List<Piece> whiteTakenPieces = new ArrayList<>();
         final List<Piece> blackTakenPieces = new ArrayList<>();
@@ -70,11 +75,12 @@ public class TakenPiecesPanel extends JPanel {
         for(final Piece takenPiece : whiteTakenPieces){
             try {
                 final BufferedImage image = ImageIO.read(new File("art/pieces/" +
-                                                            takenPiece.getPieceAlliance().toString().substring(0,1) + " "
-                                                          + takenPiece.toString()));
+                                                            takenPiece.getPieceAlliance().toString().substring(0,1) + ""
+                                                          + takenPiece + ".gif"));
                 final ImageIcon icon = new ImageIcon(image);
-                final JLabel imageLabel = new JLabel();
-                this.southPanel.add(imageLabel);
+                final JLabel imageLabel = new JLabel(new ImageIcon(icon.getImage().getScaledInstance(
+                        icon.getIconWidth() - 15, icon.getIconWidth() - 15, Image.SCALE_SMOOTH)));
+                this.blackPanel.add(imageLabel);
             } catch (final IOException e){
                 e.printStackTrace();
             }
@@ -83,11 +89,12 @@ public class TakenPiecesPanel extends JPanel {
         for(final Piece takenPiece : blackTakenPieces){
             try {
                 final BufferedImage image = ImageIO.read(new File("art/pieces/" +
-                        takenPiece.getPieceAlliance().toString().substring(0,1) + " "
-                        + takenPiece.toString()));
+                        takenPiece.getPieceAlliance().toString().substring(0,1) + ""
+                        + takenPiece.toString() + ".gif"));
                 final ImageIcon icon = new ImageIcon(image);
-                final JLabel imageLabel = new JLabel();
-                this.northPanel.add(imageLabel);
+                final JLabel imageLabel = new JLabel(new ImageIcon(icon.getImage().getScaledInstance(
+                        icon.getIconWidth() - 15, icon.getIconWidth() - 15, Image.SCALE_SMOOTH)));
+                this.whitePanel.add(imageLabel);
             } catch (final IOException e){
                 e.printStackTrace();
             }
